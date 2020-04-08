2543b36c102a62327ca9cfec533fd312063960e3
Roelant Vos <roelant@ravos.nl>
2020-04-08 12:14:14 +1000

    Added default and null value suppression to various Json elements. This prevents bloating of the Json files when serialised.

--- ClassLibrary/DataWarehouseAutomation/DataWarehouseAutomation/DataItem.cs
+++ ClassLibrary/DataWarehouseAutomation/DataWarehouseAutomation/DataItem.cs
@@ -9,18 +9,32 @@ namespace DataWarehouseAutomation
     {
         [JsonProperty]
         public string name { get; set; } // Mandatory
+
+        [JsonProperty("dataType", NullValueHandling = NullValueHandling.Ignore, DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string dataType { get; set; }
+
+        [JsonProperty("characterLength", NullValueHandling = NullValueHandling.Ignore, DefaultValueHandling = DefaultValueHandling.Ignore)]
         public int characterLength { get; set; }
+
+        [JsonProperty("numericPrecision", NullValueHandling = NullValueHandling.Ignore, DefaultValueHandling = DefaultValueHandling.Ignore)]
         public int numericPrecision { get; set; }
+
+        [JsonProperty("numericScale", NullValueHandling = NullValueHandling.Ignore, DefaultValueHandling = DefaultValueHandling.Ignore)]
         public int numericScale { get; set; }
+
+        [JsonProperty("ordinalPosition", NullValueHandling = NullValueHandling.Ignore, DefaultValueHandling = DefaultValueHandling.Ignore)]
         public int ordinalPosition { get; set; }
+
+        [JsonProperty("isPrimaryKey", NullValueHandling = NullValueHandling.Ignore, DefaultValueHandling = DefaultValueHandling.Ignore)]
         public bool isPrimaryKey { get; set; }
+
+        [JsonProperty("isHardCodedValue", NullValueHandling = NullValueHandling.Ignore, DefaultValueHandling = DefaultValueHandling.Ignore)]
         public bool isHardCodedValue { get; set; }
 
         /// <summary>
         /// Free-form and optional classification for the Data Item for use in generation logic (evaluation).
         /// </summary>
-        [JsonProperty("dataItemClassification")]
+        [JsonProperty("dataItemClassification", NullValueHandling = NullValueHandling.Ignore, DefaultValueHandling = DefaultValueHandling.Ignore)]
         public List<Classification> dataItemClassification { get; set; }
     }
 }
