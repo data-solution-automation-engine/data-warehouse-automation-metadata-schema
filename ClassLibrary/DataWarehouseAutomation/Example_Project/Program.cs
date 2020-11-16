30acfa774f23a2c93472790bb0d086b8dd70d525
Roelant Vos <roelant@ravos.nl>
2020-11-16 14:40:30 +1000

    Added sapmle helper/plug-in for the templating engine.

--- ClassLibrary/DataWarehouseAutomation/Example_Project/Program.cs
+++ ClassLibrary/DataWarehouseAutomation/Example_Project/Program.cs
@@ -10,6 +10,8 @@ namespace Example_Handlebars
     {
         static void Main(string[] args)
         {
+            HandleBarsHelpers.RegisterHandleBarsHelpers();
+
             // Local variables
             string stringTemplate = "" ;
             string jsonInput = "";
@@ -24,45 +26,7 @@ namespace Example_Handlebars
             result = template(deserialisedMapping);
             Console.WriteLine(result);
 
-            // Example using extensions
-            stringTemplate = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Templates\TemplateSampleBasicWithExtensions.handlebars");
-            template = Handlebars.Compile(stringTemplate);
-            jsonInput = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Metadata\sampleBasicWithExtensions.json");
-            deserialisedMapping = JsonConvert.DeserializeObject<DataObjectMappings>(jsonInput);
-            result = template(deserialisedMapping);
-            Console.WriteLine(result);
 
-            // Example using more than one mapping at data item level
-            stringTemplate = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Templates\TemplateSampleMultipleDataItemMappings.handlebars");
-            template = Handlebars.Compile(stringTemplate);
-            jsonInput = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Metadata\sampleMultipleDataItemMappings.json");
-            deserialisedMapping = JsonConvert.DeserializeObject<DataObjectMappings>(jsonInput);
-            result = template(deserialisedMapping);
-            Console.WriteLine(result);
-
-            // Example generating DDL statements
-            stringTemplate = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Templates\TemplateSampleSimpleDDL.handlebars");
-            template = Handlebars.Compile(stringTemplate);
-            jsonInput = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Metadata\sampleSimpleDDL.json");
-            deserialisedMapping = JsonConvert.DeserializeObject<DataObjectMappings>(jsonInput);
-            result = template(deserialisedMapping);
-            Console.WriteLine(result);
-
-            // Example using OneOf / swapping a source for a query
-            stringTemplate = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Templates\TemplateSampleCalculation.handlebars");
-            template = Handlebars.Compile(stringTemplate);
-            jsonInput = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Metadata\sampleCalculation.json");
-            deserialisedMapping = JsonConvert.DeserializeObject<DataObjectMappings>(jsonInput);
-            result = template(deserialisedMapping);
-            Console.WriteLine(result);
-
-            // Data Vault Satellite example
-            stringTemplate = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Templates\TemplateSatelliteView.handlebars");
-            template = Handlebars.Compile(stringTemplate);
-            jsonInput = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Metadata\sampeVDW_Sat_Customer_v161.json");
-            deserialisedMapping = JsonConvert.DeserializeObject<DataObjectMappings>(jsonInput);
-            result = template(deserialisedMapping);
-            Console.WriteLine(result);
 
             Console.ReadKey();
         }
