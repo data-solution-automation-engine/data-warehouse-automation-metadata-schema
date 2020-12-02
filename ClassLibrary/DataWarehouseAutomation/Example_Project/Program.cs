0eab4702c3bce271a4d1eacb22549d0b9d6bd9bd
Roelant Vos <roelant@ravos.nl>
2020-12-02 15:01:02 +1000

    Re-enabled all examples, hopefully for the last time.

--- ClassLibrary/DataWarehouseAutomation/Example_Project/Program.cs
+++ ClassLibrary/DataWarehouseAutomation/Example_Project/Program.cs
@@ -13,7 +13,7 @@ namespace Example_Handlebars
             HandleBarsHelpers.RegisterHandleBarsHelpers();
 
             // Local variables
-            string stringTemplate = "" ;
+            string stringTemplate = "";
             string jsonInput = "";
             string result = "";
             DataObjectMappings deserialisedMapping = new DataObjectMappings();
@@ -26,7 +26,45 @@ namespace Example_Handlebars
             result = template(deserialisedMapping);
             Console.WriteLine(result);
 
+            // Example using extensions
+            stringTemplate = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Templates\TemplateSampleBasicWithExtensions.handlebars");
+            template = Handlebars.Compile(stringTemplate);
+            jsonInput = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Metadata\sampleBasicWithExtensions.json");
+            deserialisedMapping = JsonConvert.DeserializeObject<DataObjectMappings>(jsonInput);
+            result = template(deserialisedMapping);
+            Console.WriteLine(result);
 
+            // Example using more than one mapping at data item level
+            stringTemplate = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Templates\TemplateSampleMultipleDataItemMappings.handlebars");
+            template = Handlebars.Compile(stringTemplate);
+            jsonInput = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Metadata\sampleMultipleDataItemMappings.json");
+            deserialisedMapping = JsonConvert.DeserializeObject<DataObjectMappings>(jsonInput);
+            result = template(deserialisedMapping);
+            Console.WriteLine(result);
+
+            // Example generating DDL statements
+            stringTemplate = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Templates\TemplateSampleSimpleDDL.handlebars");
+            template = Handlebars.Compile(stringTemplate);
+            jsonInput = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Metadata\sampleSimpleDDL.json");
+            deserialisedMapping = JsonConvert.DeserializeObject<DataObjectMappings>(jsonInput);
+            result = template(deserialisedMapping);
+            Console.WriteLine(result);
+
+            // Example using OneOf / swapping a source for a query
+            stringTemplate = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Templates\TemplateSampleCalculation.handlebars");
+            template = Handlebars.Compile(stringTemplate);
+            jsonInput = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Metadata\sampleCalculation.json");
+            deserialisedMapping = JsonConvert.DeserializeObject<DataObjectMappings>(jsonInput);
+            result = template(deserialisedMapping);
+            Console.WriteLine(result);
+
+            // Data Vault Satellite example
+            stringTemplate = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Templates\TemplateSatelliteView.handlebars");
+            template = Handlebars.Compile(stringTemplate);
+            jsonInput = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Sample_Metadata\sampeVDW_Sat_Customer_v161.json");
+            deserialisedMapping = JsonConvert.DeserializeObject<DataObjectMappings>(jsonInput);
+            result = template(deserialisedMapping);
+            Console.WriteLine(result);
 
             Console.ReadKey();
         }
---
+++ ClassLibrary/DataWarehouseAutomation/RunDwhAutomation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RunDwhAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
