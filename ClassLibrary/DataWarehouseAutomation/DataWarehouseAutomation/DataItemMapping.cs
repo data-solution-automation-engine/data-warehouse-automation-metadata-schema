ef61623d8424fd6565822bb5cec4c40ab6f41932
Roelant Vos <roelant@ravos.nl>
2023-05-12 14:04:37 +1000

    DataItemMapping

--- ClassLibrary/DataWarehouseAutomation/DataWarehouseAutomation/DataItemMapping.cs
+++ ClassLibrary/DataWarehouseAutomation/DataWarehouseAutomation/DataItemMapping.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using System.Xml.Linq;
 
 namespace DataWarehouseAutomation;
 
@@ -9,6 +10,13 @@ namespace DataWarehouseAutomation;
 public class DataItemMapping
 {
 #nullable enable
+
+    /// <summary>
+    /// Optional identifier as a string value to allow various identifier approaches.
+    /// </summary>
+    [JsonPropertyName("id")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public string Id { get; set; } = string.Empty;
 
     [JsonPropertyName("sourceDataItems")]
     public List<dynamic> SourceDataItems { get; set; } = new();
@@ -23,4 +31,32 @@ public class DataItemMapping
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public List<Extension>? Extensions { get; set; }
 
+    #region Methods
+    /// <summary>
+    /// Use this method to assert an object as a DataItemMapping (or not).
+    /// </summary>
+    /// <param name="o"></param>
+    /// <returns></returns>
+    public override bool Equals(object? o)
+    {
+        var other = o as DataItemMapping;
+        return other?.Id == Id;
+    }
+
+    /// <summary>
+    /// Override to get a hash value that represents the identifier. 
+    /// </summary>
+    /// <returns></returns>
+    public override int GetHashCode() => Id.GetHashCode();
+
+    /// <summary>
+    /// String override so that the object returns its value ('name of the target data item').
+    /// When an instance of this class is passed to a method that expects a string, the ToString() method will be called implicitly to convert the object to a string, and the value of the Data Item "Name" property will be returned.
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return TargetDataItem.Name;
+    }
+    #endregion
 }
